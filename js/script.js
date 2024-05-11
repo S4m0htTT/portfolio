c65827df2c999c44a61a7e540d078251f6373a84
S4m0htTT <thomashenry7750@gmail.com>
2024-05-11 17:51:04 +0200

    header

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var nav = document.getElementById('header');
+window.addEventListener('scroll', () =>{
+    if (window.scrollY > 0){
+        nav.classList.add('bg-black');
+    }else{
+        nav.classList.remove('bg-black');
+    }
+});
+
+function select(name){
+    var links = document.querySelectorAll('.nav-btn');
+    links.forEach(link => {
+        link.classList.remove('select');
+    });
+    name.classList.add('select');
+}
