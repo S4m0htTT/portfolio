d9906057c90ca54d20378e3d2818607bef20666f
S4m0htTT <thomashenry7750@gmail.com>
2024-05-16 07:24:14 +0200

    fini manque responsive

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 var nav = document.getElementById('header');
-window.addEventListener('scroll', () =>{
-    if (window.scrollY > 0){
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
         nav.classList.add('bg-black');
-    }else{
+    } else {
         nav.classList.remove('bg-black');
     }
 });
 
-function select(name){
+function select(name) {
     var links = document.querySelectorAll('.nav-btn');
     links.forEach(link => {
         link.classList.remove('select');
@@ -15,7 +15,29 @@ function select(name){
     name.classList.add('select');
 }
 
-function copietxt(){
-    navigator.clipboard.writeText('thomashenry7750@gmail.com').then(()=>
-    alert('Copied!'));
+function copietxt() {
+    let popup = document.getElementById('popup-mail');
+    navigator.clipboard.writeText('thomashenry7750@gmail.com')
+    popup.style.display = 'block';
+    setTimeout(() => {
+        popup.style.display = 'none';
+    }, 5000);
 }
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    var links = document.querySelectorAll('.nav-btn');
+    links.forEach(link => {
+        link.classList.remove('select');
+    });
+    var hash = window.location.hash;
+    if (hash == '#Apropos') {
+        document.getElementById('propos').classList.add('select');
+    } else if (hash == '#projets') {
+        document.getElementById('projet').classList.add('select');
+    } else if (hash == '#contact') {
+        document.getElementById('contactBtn').classList.add('select');
+    }else{
+        document.getElementById('accueil').classList.add('select');
+    }
+});
