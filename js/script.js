e0ba239e00b800061b58b2282e88577d3a99ba1b
S4m0htTT <thomashenry7750@gmail.com>
2024-07-29 10:44:11 +0200

    test génération div proj avec json

--- js/script.js
+++ js/script.js
@@ -61,3 +61,19 @@ document.addEventListener("DOMContentLoaded", function (event) {
         document.getElementById('accueil').classList.add('select');
     }
 });
+
+function fetchJSONData() {
+    fetch("./projet.json")
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error
+                    (`HTTP error! Status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then((data) => 
+              console.log(data))
+        .catch((error) => 
+               console.error("Unable to fetch data:", error));
+}
+// fetchJSONData();
